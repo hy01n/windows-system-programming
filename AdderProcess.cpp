727ed59e35113096e0e8de6ac9f4f3e5150b6521
Hyoin Jeong <hobbyhyoin@naver.com>
2022-01-19 21:18:57 +0900

    Process 생성과 소멸

---
+++ AdderProcess.cpp
@@ -0,0 +1,22 @@
+﻿/*
+
+	AdderProcess.cpp
+	main 함수 전달인자 덧셈
+
+*/
+
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	DWORD val1, val2;
+	val1 = _ttoi(argv[1]);
+	val2 = _ttoi(argv[2]);
+
+	_tprintf(_T("%d + %d = %d\n"), val1, val2, val1 + val2);
+
+	_gettchar();
+	return 0;
+}
